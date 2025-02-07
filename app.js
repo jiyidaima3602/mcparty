5484a3deecf688f6a5c79f2c967e51234487ec4b
jiyidaima3602 <2193218694@qq.com>
2025-02-07 23:22:03 +0800

    1

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-// 初始化Supabase
-const supabase = createClient(
+// ======================
+// 初始化Supabase客户端
+// ======================
+const { createClient } = supabase;
+const supabaseClient = createClient(
   'https://jzpcrdvffrpdyuetbefb.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6cGNyZHZmZnJwZHl1ZXRiZWZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg5MzY1MzQsImV4cCI6MjA1NDUxMjUzNH0.0IRrxVdeKtbrfFyku0CvXsyeAtYp1mXXxLvyEQ6suTM'
 );
@@ -10,7 +13,7 @@ const supabase = createClient(
 
 // 加载并显示帖子列表
 function loadPosts() {
-  supabase
+  supabaseClient
     .from('posts')
     .select('*')
     .then(({ data: posts }) => {
